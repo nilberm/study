809e2cbf3b7ddae412710e52b2e1cce56dd7ea50
nilberm <nilber16@outlook.com>
2022-05-16 11:31:38 -0300

    events

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 
 class Header extends Component {
+  inputChangeHandler(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <header>
         <div className="logo">Logo</div>
-        <input />
+        <input onChange={this.inputChangeHandler} />
       </header>
     );
   }
